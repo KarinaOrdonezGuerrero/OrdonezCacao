fc52456055ad02f2b3d51e325cfccdb6401b6811
Karina Michelle Ordóñez Guerrero <74227843+KarinaOrdonezGuerrero@users.noreply.github.com>
2020-11-16 01:20:30 -0500

    Update OrdonezGCacao.R

--- OrdonezGCacao.R
+++ OrdonezGCacao.R
@@ -1,1 +1,14 @@
-install.packages('tidyverse')
+#install.packages('tidyverse')
+install.packages("RPostgreSQL")
+require("RPostgreSQL")
+pw <- { "12345678"}
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname = "ordonezgcacao",
+                 host = "localhost", port = 5432,
+                 user = "karinaordonezg", password = pw)
+out <- dbGetQuery(con,"select *from plantacion")
+outi <- dbGetQuery(con,"select *from produccion")
+#####VALIDO
+library(DBI)
+con <- dbConnect(odbc::odbc(),database="ordonezgcacao", .connection_string = "Driver={PostgreSQL Unicode};", timeout = 10)
+result <- dbSendQuery(con,"select *from plantacion)
